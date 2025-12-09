e51851352dc839e5c99ae1ed947ac51c84159765
dennowiggle <60706474+dennowiggle@users.noreply.github.com>
2025-12-09 12:34:38 -0800

    Initial release files

---
+++ release/PETER-Programmer-v0.1.3686/flash.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+SET "comport="
+SET "baudrate="
+CALL SET "comport=%%1"
+CALL SET "baudrate=%%2"
+IF defined comport GOTO :baudrate
+
+:nocomport
+@ECHO Usage: flash ^[COM_PORT^] ^<BAUDRATE^>
+exit /b %ERRORLEVEL%
+
+:baudrate
+IF defined baudrate GOTO :flash
+CALL SET "baudrate=921600"
+
+:flash
+ECHO Flashing to ESP32-S3.
+REM Booloader
+".\esptool.exe" --chip esp32s3 --port %comport% --baud %baudrate% --before default-reset --after no-reset   write-flash -e -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0x0 ".\bootloader.bin" 
+REM Partions
+".\esptool.exe" --chip esp32s3 --port %comport% --baud %baudrate% --before no-reset      --after no-reset   write-flash -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0x8000  ".\partitions.bin" 
+REM Boot App
+".\esptool.exe" --chip esp32s3 --port %comport% --baud %baudrate% --before no-reset      --after no-reset   write-flash -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0xe000   ".\boot_app0.bin" 
+REM firmware
+".\esptool.exe" --chip esp32s3 --port %comport% --baud %baudrate% --before no-reset      --after no-reset   write-flash -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0x10000  ".\firmware.bin" 
+REM Filesystem
+".\esptool.exe" --chip esp32s3 --port %comport% --baud %baudrate% --before no-reset      --after hard-reset write-flash -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0x00670000 ".\littlefs.bin"
+exit
+
---
+++ release/PETER-Programmer-v0.1.3686/flash.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+BAUD=921600
+
+if [ "$#" -eq 0 ] || [ "$#" -gt 2 ]; then
+    echo Usage: flash [SERIAL_PORT] \<BAUDRATE\>
+    exit 1
+fi
+
+if [ "$#" -eq 1 ]; then
+    SERIAL=$1
+fi
+
+if [ "$#" -eq 2 ]; then
+    SERIAL=$1
+    BAUD=$2
+fi
+echo Flashing to ESP32-S3.
+# Booloader
+esptool --chip esp32s3 --port "$SERIAL" --baud $BAUD --before default-reset --after no-reset   write-flash -e -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0x0 "./bootloader.bin" 
+# Partions
+esptool --chip esp32s3 --port "$SERIAL" --baud $BAUD --before no-reset      --after no-reset   write-flash -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0x8000  "./partitions.bin" 
+# Boot App
+esptool --chip esp32s3 --port "$SERIAL" --baud $BAUD --before no-reset      --after no-reset   write-flash -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0xe000   "./boot_app0.bin" 
+# firmware
+esptool --chip esp32s3 --port "$SERIAL" --baud $BAUD --before no-reset      --after no-reset   write-flash -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0x10000  "./firmware.bin" 
+# Filesystem
+esptool --chip esp32s3 --port "$SERIAL" --baud $BAUD --before no-reset      --after watchdog-reset write-flash -z --flash-mode dio --flash-freq 80m --flash-size 8MB 0x00670000 "./littlefs.bin"
